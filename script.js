b948ac8695d1f431c68ebb673eb5cd311407616b
Chavelyo3 <roxette.banos@gmail.com>
2020-10-27 18:12:12 -0500

    Uploading finally

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables 
+let questionsEl = document.getElementById("questions");
+let choiceEl = document.getElementById("choices");
+let startBtn = document.querySelector("#start");
+let submitBtn = document.getElementById("submit");
+let timerEl = document.getElementById("timer");
+let nameEl = document.getElementById("name");
+let returnEl = document.getElementById("return");
+
+
+
